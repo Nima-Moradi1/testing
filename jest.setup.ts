90260d761d50615b6e2e708b6e7c9d8ec1a3a219
nima moradi <mnima8100@gmail.com>
2025-03-07 10:14:05 +0330

    set up MSW server for API mocking in Jest tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,18 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom' ;
+import { server } from './mocks/server';
+
+//Before every unit test gets fired, this method will run first
+beforeAll(() => {
+    //? Start the server before running tests
+    server.listen();
+})
+
+afterEach(()=> {
+    //? We reset the handlers after each test because we don't want the previous handler data to be carried into next ones.
+    server.resetHandlers()
+})
+
+//After all the tests are done, close the server (looks like a cleanup function)
+afterAll(() => {
+    server.close()
+})
