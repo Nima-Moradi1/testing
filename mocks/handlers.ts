85151f76bafe7460be56b6d4c10a52d4ef7f3b07
nima moradi <mnima8100@gmail.com>
2025-03-07 12:35:42 +0330

    msw V2 changes (no more ctx > httpResponse)

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,12 +1,11 @@
 //? This file is where we're going to make endpoint handlers and mock all of our endpoints
 
-import {http} from 'msw'
+import {http, HttpResponse} from 'msw'
 
 export const handlers = [
-    /*@ts-expect-error : no types inferred for req,res,ctx*/
-    http.get('/api/users' , (req,res,ctx) => {
-        return res(
-            ctx.json([{id:1 , username : 'nimooli'}])
+    http.get('/api/users' , () => {
+        return HttpResponse.json(
+         {id:1 , username : 'Nimooli'}
         )
     })
 ]
